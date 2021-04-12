5a42ef14bd6b8811e6bc6926e5e78262cef06bdb
Alex Wong <alex.d.wong.21@dartmouth.edu>
2021-04-12 18:33:40 -0400

    test site

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+$('#main').html('Here we go!');
+
+let count = 0;
+
+setInterval(() => {
+  count += 1;
+  $('#main').html(`You have been here ${count} seconds.`);
+}, 1000);
